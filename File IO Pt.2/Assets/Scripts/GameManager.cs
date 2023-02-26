94230bdb68003c74421537a93abfb2e2a644b828
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-02-26 09:30:41 -0500

    Deleted template, added my own project

--- File IO Pt.2/Assets/Scripts/GameManager.cs
+++ File IO Pt.2/Assets/Scripts/GameManager.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExampleScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    public int var1;
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("var1: " + var1);
+        
     }
 
     // Update is called once per frame
